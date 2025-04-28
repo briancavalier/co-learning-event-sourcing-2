86a322d810db3883745433ea4c004bcaecc8e8e2
Brian Cavalier <brian@briancavalier.com>
2025-04-27 20:22:32 -0400

    Colearning: event sourcing part 2 collab
    Co-authored-by: Evan <etmoore@users.noreply.github.com>
    Co-authored-by: adam-vitally <adam-vitally@users.noreply.github.com>
    Co-authored-by: Jared Cacurak <jared.cacurak@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,117 @@
-console.log('Hello, event sourcing!')
+type LeaderboardEvent = LeaderboardCreated | LeaderboardWon | UserExercised | UserJoined
+
+type LeaderboardCreated = {
+    type: 'leaderboard-created'
+    leaderboardId: string
+    name: string
+    description: string,
+    goal: number // minutes of elevated heart rate
+    timestamp: number
+}
+
+type LeaderboardWon = {
+    type: 'leaderboard-won'
+    leaderboardId: string
+    winnerId: string
+    timestamp: number
+}
+
+type UserExercised = {
+    type: 'user-exercised'
+    userId: string
+    minutes: number
+    timestamp: number
+}
+
+type UserJoined = {
+    type: 'user-joined'
+    userId: string
+    leaderboardId: string
+    timestamp: number
+}
+
+type EventStore<A> = A[]
+
+const putEvent = <A>(eventStore: EventStore<A>, event: A): void => {
+    eventStore.push(event)
+}
+
+const getEvents = <A>(eventStore: EventStore<A>): readonly A[] => {
+    return eventStore
+}
+
+const usersOnLeaderboard = (eventStore: EventStore<LeaderboardEvent>, leaderboardId: string): Set<string> => {
+    const users = new Set<string>()
+    for (const event of eventStore) {
+        if (event.type === 'user-joined' && event.leaderboardId === leaderboardId) {
+            users.add(event.userId)
+        }
+    }
+    return users
+}
+
+type UserOnLeaderboard = {
+    userId: string
+    minutes: number
+}
+
+const getLeaderboardUsersRanked = (eventStore: EventStore<LeaderboardEvent>, leaderboardId: string): UserOnLeaderboard[] => {
+    const users = usersOnLeaderboard(eventStore, leaderboardId)
+    const ranked = new Map<string, number>()
+    for (const event of eventStore) {
+        if (event.type === 'user-exercised' && users.has(event.userId)) {
+            const r = ranked.get(event.userId) ?? 0
+            ranked.set(event.userId, r + event.minutes)
+        }
+    }
+
+    return [...ranked.entries()].map(([userId, minutes]) => ({ userId, minutes })).sort((a, b) => b.minutes - a.minutes)
+}
+
+const eventStore: EventStore<LeaderboardEvent> = []
+
+putEvent(eventStore, {
+    type: 'leaderboard-created',
+    leaderboardId: '1',
+    name: 'Leaderboard 1',
+    description: 'Description 1',
+    goal: 100,
+    timestamp: Date.now()
+})
+
+putEvent(eventStore, {
+    type: 'user-joined',
+    userId: 'user-1',
+    leaderboardId: '1',
+    timestamp: Date.now()
+})
+
+putEvent(eventStore, {
+    type: 'user-joined',
+    userId: 'user-2',
+    leaderboardId: '1',
+    timestamp: Date.now()
+})
+
+putEvent(eventStore, {
+    type: 'user-exercised',
+    userId: 'user-1',
+    minutes: 50,
+    timestamp: Date.now()
+})
+
+putEvent(eventStore, {
+    type: 'user-exercised',
+    userId: 'user-2',
+    minutes: 60,
+    timestamp: Date.now()
+})
+
+putEvent(eventStore, {
+    type: 'user-exercised',
+    userId: 'user-1',
+    minutes: 50,
+    timestamp: Date.now()
+})
+
+console.log(getLeaderboardUsersRanked(eventStore, '1'))
